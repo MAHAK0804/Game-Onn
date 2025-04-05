8f727c30354a11332524e44c0fb5c74e4d8f079c
SwapKul <skulshreshtha182@gmail.com>
2025-04-05 17:12:12 +0530

    init

---
+++ src/constants/routes.ts
@@ -0,0 +1,16 @@
+import { IHeaderLink } from "@/interfaces";
+
+export const headerLinks: IHeaderLink[] = [
+  {
+    name: "Home",
+    route: "/",
+  },
+  {
+    name: "Categories",
+    route: "/categories",
+  },
+  {
+    name: "Venues",
+    route: "/venues",
+  },
+];
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
+import Footer from "@/layouts/Footer";
+import Header from "@/layouts/Header";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
 }
