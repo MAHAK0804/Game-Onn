89b564c842c1c0a9d157b584230dd27a590cc9e0
MAHAK0804 <mahak191713@gmail.com>
2025-04-08 14:55:27 +0530

    Category section optimize

---
+++ src/constants/Category.ts
@@ -0,0 +1,11 @@
+import soccer from "../../public/assets/soccerBall.png";
+import closeUpBasket from "../../public/assets/closeBasketBall.png";
+import sportCar from "../../public/assets/sportCar.png";
+import redPing from "../../public/assets/redPing.png";
+
+export const categoryData = [
+  { text: "football", img: soccer },
+  { text: "BasketBall", img: closeUpBasket },
+  { text: "Car Sport", img: sportCar },
+  { text: "Table Tennis", img: redPing },
+];
